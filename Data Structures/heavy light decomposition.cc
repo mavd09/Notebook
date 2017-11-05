a279e840beaad8c36d02d3b7634b320958d7d63c
mavd09 <mavd09@hotmail.com>
2017-11-05 11:54:44 -0500

    Fixed HLD.

--- Data Structures/heavy light decomposition.cc
+++ Data Structures/heavy light decomposition.cc
@@ -1,89 +1,100 @@
-struct Data_Node {
-  int prnt, dpth, value, id, sz, chain;
-  Data_Node( ) : 
-    prnt( 0 ), dpth( 0 ), value( 0 ), 
-    id( 0 ), sz( 1 ), chain( 0 ) { }
-};
-struct Heavy_Light_Decomposition {
-  int n, node_counted, cur_chain;
-  vvi G, A;
-  vector< Data_Node > data_node;
-  vi chain_head;
-  Segment_Tree st;
-  Heavy_Light_Decomposition( int n, vi& value ) : 
-    n( n ), G( n ), A( n, vi( LOG2 ) ), 
-    data_node( n, Data_Node( ) ), chain_head( n, -1 ), st( 0, n-1 ) {
-    node_counted = cur_chain = 0;
-    for( int i = 0; i < n; i++ )
-      data_node[ i ].value = value[ i ];
-  }
-  void add_edge( int u, int v ) {
-    G[ u ].PB( v );
-    G[ v ].PB( u );
-  }
-  void set_value( int u, int v ) {
-    data_node[ u ].value = v;
-    st.update( data_node[ u ].id, data_node[ u ].value );
-  }
-  void dfs( int u, int d ) {
-    data_node[ u ].dpth = d;
-    for( int i = 0; i < int( G[ u ].size( ) ); i++ ) {
-      int v = G[ u ][ i ];
-      if( v == data_node[ u ].prnt )
-        continue;
-      data_node[ v ].prnt = A[ v ][ 0 ] = u;
-      dfs( v, d+1 );
-      data_node[ u ].sz += data_node[ v ].sz;
+struct Edge { int v, c, idx; };
+
+int n;
+vector< Edge > graph[ MAXN ];
+int base_array[ MAXN ], ptr;
+int n_chain, chain_ind[ MAXN ], chain_head[ MAXN ], pos_base[ MAXN ];
+int depth[ MAXN ], dp[ LOG2 ][ MAXN ], other_end[ MAXN ], subsize[ MAXN ];
+int st[ MAXN*4 ];
+
+void init( ) {
+  n_chain = ptr = 0;
+  for( int i = 0; i < n; ++i ) {
+    graph[i].clear();
+    chain_head[i] = -1;
+    for( int j = 0; j < LOG2; ++j ) {
+      dp[j][i] = -1;
     }
   }
-  void build_hld( int u ) {
-    if( chain_head[ cur_chain ] == -1 )
-      chain_head[ cur_chain ] = u;
-    data_node[ u ].chain = cur_chain;
-    data_node[ u ].id = node_counted++;
-    st.update( data_node[ u ].id, data_node[ u ].value );
-    int best_child = -1;
-    for( auto& v : G[ u ] ) {
-      if( v == data_node[ u ].prnt )
-        continue;
-      if( best_child == -1 || ( data_node[ best_child ].sz < data_node[ v ].sz ) )
-        best_child = v;
-    }
-    if( best_child != -1 )
-      build_hld( best_child );
-    for( auto& v : G[ u ] ) {
-      if( v == data_node[ u ].prnt || v == best_child )
-        continue;
-      cur_chain++;
-      build_hld( v );
-    }
-  }
-  void build( ) {
-    dfs( 0, 0 );
-    build_lca( );
-    build_hld( 0 );
-  }
-  int query_hld_util( int u, int a ) {
-    int r = data_node[ u ].value;
-    int chain_u, chain_a = data_node[ a ].chain;
-    while( true ) {
-      chain_u = data_node[ u ].chain;
-      if( chain_u == chain_a ) {
-        if( u != a )
-          r = __gcd( r, st.query( data_node[ u ].id, data_node[ a ].id ) );
-        else
-          r = __gcd( r, data_node[ u ].value );
-        break;
+}
+
+void make_tree( int node, int s, int e );
+void update_tree( int node, int s, int e, int x, int val );
+int query_tree( int node, int s, int e, int l, int r );
+void create_LCA( );
+int LCA( int u, int v );
+
+int query_up(int u, int v) {
+  if( u == v ) return 0;
+  int uchain, vchain = chain_ind[v], ans = -1;
+  while( true ) {
+    uchain = chain_ind[u];
+    if( uchain == vchain ) {
+      if( u != v ) {
+        ans = max( ans, query_tree( 1, 0, n-1, pos_base[v]+1, pos_base[u] ) );
       }
-      else {
-        r = __gcd( r, st.query( data_node[ chain_head[ chain_u ] ].id, data_node[ u ].id ) );
-        u = data_node[ chain_head[ chain_u ] ].prnt;
-      }
+      return ans;
     }
-    return r;
+    ans = max( ans, query_tree( 1, 0, n-1, pos_base[ chain_head[uchain] ], pos_base[u] ) );
+    u = chain_head[uchain];
+    u = dp[0][u];
   }
-  int query_hld( int u, int v ) {
-    int lca = query_lca( u, v );
-    return __gcd( query_hld_util( u, lca ), query_hld_util( v, lca ) );
+  return -1;
+}
+
+int query( int u, int v ) {
+  int lca = LCA( u, v );
+  return max( query_up( u, lca ), query_up( v, lca ) );
+}
+
+void change( int i, int val ) {
+  int u = other_end[i];
+  update_tree( 1, 0, n-1, pos_base[u], val );
+}
+
+void HLD( int u, int p = -1, int c = -1 ) {
+  if( chain_head[n_chain] == -1 ) {
+    chain_head[n_chain] = u;
   }
-};
+  chain_ind[u] = n_chain;
+  pos_base[u] = ptr;
+  base_array[ptr++] = c;
+  int child = -1, ncost;
+  for( int i = 0; i < SIZE(graph[u]); ++i ) {
+    Edge& e = graph[u][i];
+    if( e.v == p ) continue;
+    if( child == -1 || subsize[child] < subsize[e.v] ) {
+      child = e.v;
+      ncost = e.c;
+    }
+  }
+  if( child != -1 ) {
+    HLD( child, u, ncost );
+  }
+  for( int i = 0; i < SIZE(graph[u]); ++i ) {
+    Edge& e = graph[u][i];
+    if( e.v == p || e.v == child ) continue;
+    n_chain++;
+    HLD( e.v, u, e.c );
+  }
+}
+
+void dfs( int u, int p = -1 ) {
+  dp[0][u] = p;
+  depth[u] = ( p == -1 ? 0 : depth[p]+1 );
+  subsize[u] = 1;
+  for( int i = 0; i < SIZE(graph[u]); ++i ) {
+    Edge& e = graph[u][i];
+    if( e.v == p ) continue;
+    other_end[ e.idx ] = e.v;
+    dfs( e.v, u );
+    subsize[u] += subsize[e.v];
+  }
+}
+
+void create_HLD( ) {
+  dfs( 0 );
+  HLD( 0 );
+  make_tree( 1, 0, n-1 );
+  create_LCA( );
+}
